3aff84d622a4e533abe54b2ff1f2b2c9d0025960
jefflee168 <86116193@qq.com>
2019-05-11 08:42:43 +0000

    change file nginx_web_error.py

--- nginx_web_error.py
+++ nginx_web_error.py
@@ -1,5 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # coding: utf8
+
+# 此脚本用于统计访问nginx的出错率，脚本用python2
 
 from __future__ import print_function
 
@@ -11,12 +13,12 @@ with open('/var/log/nginx/access.log') as f:
         d.setdefault(key,0)
         d[key] += 1
 
-sum_requrests = 0
+sum_requests = 0
 error_requests = 0
 
 for key, val in d.iteritems():
     if int(key) >= 400:
         error_requests += val
-        sum_requests += val
+    sum_requests += val
 
 print("error rate: {0:.2f}%".format(error_requests * 100.0 / sum_requests))
