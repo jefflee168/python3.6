d119daf0d8d95c0fcb608c41bf1b3413570fe52e
jefflee168 <1919081933@qq.com>
2019-07-03 20:24:11 +0800

    add file socket/server02.py

---
+++ socket/server02.py
@@ -0,0 +1,23 @@
+# coding: utf8
+
+import socket
+
+HOST = ''
+PORT = 50008
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+s.bind((HOST, PORT))
+s.listen(5)
+
+conn, addr = s.accept()
+print('Connected by ', addr)
+data = conn.recv(1024)
+
+while 1:
+    command = raw_input("Enter shell command or quit: ")
+    if command == "quit": break
+    data = conn.recv(1024)
+    print(data)
+
+conn.close()
