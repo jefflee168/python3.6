c11e1155fe28afc4fede0c7afdf050f3dcd23556
jefflee168 <1919081933@qq.com>
2019-07-03 20:01:11 +0800

    update socket\client.py

--- socket/client.py
+++ socket/client.py
@@ -7,6 +7,8 @@ PORT = 50058
 str1 = "Hello world!"
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
+
+# 此处要用.encode() 转码，否则会报错 "TypeError: a bytes-like object is required, not 'str'"
 s.sendall(str1.encode())
 data = s.recv(1024)
 s.close()
